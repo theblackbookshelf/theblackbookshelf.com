cbdf758ecb2f48910dcbc3d479e6db8350449bee
Adro Rocker <me@adro.rocks>
2021-05-04 15:18:10 -0500

    Removed link to spanish site

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,9 +8,6 @@
                 <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
                     <img src="/assets/images/small_logo_revert.png" class="logo h-56 inline-block" />
                 </h2>
-                <h5 class='text-sm md:text-sm mt-10'>
-                    <a class="text-white" target="_blank" href="https://ellibreronegro.com">Spanish</a>
-                </h5>
             </div>
         </div>
     </div>
